43657addc8964a6fbc1b5e0d51247b55a0847b80
Yves Desjardins <YvesDesjardins@gmail.com>
2019-01-26 05:05:30 +0000

    initial creation

---
+++ bars.js
@@ -0,0 +1,16 @@
+//stuff to go here for barchart project
+
+//some function to get info from html DOM or jQuerry
+var getChart = function(){
+  //initialize vars, data is array, options is just holder of properties, element is the DOM/jQuerry to render to
+  var data, options, element;
+
+  //create and return the thing to the site?
+  return makeChart(data, options, element);
+}
+
+//some function to parse info and push chart (?)
+var makeChart = function(data, options, element){
+
+  //return the chart or data needed for chart
+}
